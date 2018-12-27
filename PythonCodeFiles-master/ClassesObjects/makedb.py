37ef9aa8a7dd9095b77b79d876f6eb1ccfe83c32
M.Furkan Çankaya <m.cankaya@stu.fsm.edu.tr>
2018-12-27 11:45:55 +0300

    Son Lab

--- PythonCodeFiles-master/ClassesObjects/makedb.py
+++ PythonCodeFiles-master/ClassesObjects/makedb.py
@@ -5,8 +5,17 @@ bob = Person('Bob Smith')                        # Re-create objects to be store
 sue = Person('Sue Jones', job='dev', pay=100000)
 tom = Manager('Tom Jones', 50000)
 
+#import shelve
+#db = shelve.open('persondb')                     # Filename where objects are stored
+#for object in (bob, sue, tom):                   # Use object's name attr as key
+#    db[object.name] = object                     # Store object on shelve by key
+#db.close()                                       # Close after making changes
+
+mustafa=Person('Mustafa Kıyga', job='Çaycı', pay=2020)
+hasan=Person('Hasan Kağıt')
+furkan=Manager('Furkan Çankaya',50000)
 import shelve
-db = shelve.open('persondb')                     # Filename where objects are stored
-for object in (bob, sue, tom):                   # Use object's name attr as key
-    db[object.name] = object                     # Store object on shelve by key
-db.close()                                       # Close after making changes
+db = shelve.open('bizimkilerdb')
+for object in (hasan, mustafa, furkan):
+    db[object.name]=object
+db.close()
